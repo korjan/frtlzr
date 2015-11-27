c26d9d30c621f425509584f40485f951c3d0f96f
Kamil Afsar <kamil@q42.nl>
2015-11-27 22:30:16 +0100

    Fixt interesting.

--- chromeextension/src/content_scripts/analyze.js
+++ chromeextension/src/content_scripts/analyze.js
@@ -45,12 +45,12 @@ export default function analyze() {
   //console.log('word count', words);
   everyTimeUnit(time => {
     let activeScreenSpaces = getActiveScreenSpaces(articleEl);
-    activeScreenSpaces.forEach(activeScreenSpace => {
+    activeScreenSpaces.forEach(({top, bottom, inView}) => {
       emit(
         document.location.href,
-        activeScreenSpace.top,
-        activeScreenSpace.bottom,
-        time
+        top,
+        bottom,
+        inView ? time : 0
       );
     });
   });
@@ -59,10 +59,28 @@ export default function analyze() {
 // TODO we kijken nu alleen nog maar naar het hele scherm
 function getActiveScreenSpaces(articleEl) {
   let articleBox = articleEl.getBoundingClientRect();
-  //document.scrollingElement.clientHeight
-  //document.scrollingElement.scrollTop
-  //window.innerHeight
-  return [{top:0, bottom:articleBox.height}];
+
+  let viewport = window.innerHeight;
+  let segment = 100;
+
+  let viewportTop = articleBox.top < 0 ? Math.abs(articleBox.top) : -articleBox.top;
+  let viewportBottom = viewportTop + viewport;
+
+  let activeScreenSpaces = _.range(0, articleBox.height + segment, segment)
+    .map(segmentTop => {
+      let segmentBottom = segmentTop + segment;
+
+      let inView = (segmentTop >= viewportTop && segmentTop <= viewportBottom) ||
+                  (segmentBottom >= viewportTop && segmentBottom <= viewportBottom);
+
+      return {
+        top: segmentTop,
+        bottom: segmentBottom,
+        inView
+      };
+    });
+
+  return activeScreenSpaces;
 }
 
 function emit(url, top, bottom, time) {
