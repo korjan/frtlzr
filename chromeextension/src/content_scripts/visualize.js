c40224e8123c81c7a5d545fbf15a1c22ee4d8b3c
Jasper Haggenburg <jasperh@q42.nl>
2015-11-27 15:52:05 +0100

    different animations for differents kind of bs

--- chromeextension/src/content_scripts/visualize.js
+++ chromeextension/src/content_scripts/visualize.js
@@ -6,8 +6,9 @@ export default class Visualize {
     const apiResults = this.getApiResults();
 
     // Add classes to links
-    this.setupLinks(apiResults);
-    // setTimeout(() => this.setupLinks(apiResults), 1000);
+    setTimeout(() => {
+      this.setupLinks(apiResults);
+    }, 1000);
 
     // Toggle view
     document.onkeydown = this.onKeyDown;
@@ -18,46 +19,67 @@ export default class Visualize {
     return [
       { // medium
         url: 'https://medium.com/@lawham/phonetic-table-for-trolls-f03c05dc7f6a',
-        bullshit: 1
+        bullshit: 0.8
       }, {
         url: 'https://medium.com/@Asher_Wolf/here-s-to-not-hating-other-parents-b34ee8dd8c7d',
-        bullshit: 1
+        bullshit: 0.5
       }, {
         url: 'https://medium.com/@Asher_Wolf/here-s-to-not-hating-other-parents-b34ee8dd8c7d',
-        bullshit: 1
+        bullshit: 0.8
       }, { // nu.nl
-        url: '/binnenland/4173018/supporters-ajax-stappen-rechter-aanwezigheid-bij-fc-utrecht.html',
-        bullshit: 1
+        url: '/politiek/4173073/kabinet-maakt-jaar-350-miljoen-vrij-opvang-vluchtelingen.html',
+        bullshit: 0.5
+      }, {
+        url: '/beurs/4173100/wall-street-opent-kalm-halve-handelsdag.html',
+        bullshit: 0.8
+      }, {
+        url: '/buitenland/4173080/poetin-weigert-contact-met-erdogan-.html',
+        bullshit: 0.8
+      }, {
+        url: '/politiek/4173073/kabinet-maakt-jaar-350-miljoen-vrij-opvang-vluchtelingen.html',
+        bullshit: 0.8
+      }, {
+        url: '/binnenland/4173047/geen-toename-aantal-discriminatieklachten-taboe-melden.html',
+        bullshit: 0.8
+      }, {
+        url: '/buitenland/4172855/frankrijk-herdenkt-slachtoffers-aanslagen-parijs.html',
+        bullshit: 0.2
       }, {
         url: '/binnenland/4172847/kuipverbod-en-werkstraffen-geeist-feyenoordfans-in-rome-zaak.html',
-        bullshit: 1
+        bullshit: 0.2
       }, {
-        url: '/politiek/4172867/akkoord-opvang-huisvesting-en-integratie-asielzoekers.html',
-        bullshit: 1
+        url: '/aanslag-parijs/4172882/twee-arrestaties-bij-huiszoekingen-in-belgie.html',
+        bullshit: 0.5
+      }, {
+        url: '/beurs/4173100/dunne-handel-verwacht-wall-street.html',
+        bullshit: 0.5
+      }, {
+        url: '/voetbal/4173109/van-aken-maakt-rentree-bij-heerenveen-vitesse-mist-kashia-nec.html',
+        bullshit: 0.5
       }, { // hackernews
         url: 'http://russell-j.com/0583TS.HTM',
-        bullshit: 1
+        bullshit: 0.7
       }, {
         url: 'http://alchemy.cs.washington.edu/',
-        bullshit: 1
+        bullshit: 0.7
       }, {
         url: 'https://labs.kunstmaan.be/blog/ios-continuous-delivery-with-jenkins-and-fastlane',
-        bullshit: 1
+        bullshit: 0.5
       }, {
         url: 'http://blog.nobugware.com/post/2015/listening_to_satellites_for_30_dollars/',
         bullshit: 1
       }, {
         url: 'http://engineering.linkedin.com/network-performance/tcp-over-ip-anycast-pipe-dream-or-reality',
-        bullshit: 1
+        bullshit: 0.1
       }, {
         url: 'http://nautil.us/issue/30/identity/is-farmed-salmon-really-salmon',
-        bullshit: 1
+        bullshit: 0.2
       }, {
         url: 'https://www.quantamagazine.org/20141015-at-the-far-ends-of-a-new-universal-law/',
-        bullshit: 1
+        bullshit: 0.2
       }, {
         url: 'https://news.ycombinator.com/item?id=10634732',
-        bullshit: 1
+        bullshit: 0.2
       }, {
         url: 'http://fibbing.github.io/',
         bullshit: 1
@@ -85,12 +107,11 @@ export default class Visualize {
   setupLink(apiResult, domNode) {
     domNode.classList.add('--bs--link');
 
+    console.log('bs--link-bullshit:', apiResult.bullshit)
     if (apiResult.bullshit) {
       domNode.classList.add('--bs--link-bullshit');
-    }
-
-    if (apiResult.interesting) {
-      domNode.classList.add('--bs--link-interesting');
+      domNode.classList.add(`--bs--link-bullshit-${apiResult.bullshit.toString().replace('.', '-')}`);
+      console.log(domNode.classList);
     }
   }
 
