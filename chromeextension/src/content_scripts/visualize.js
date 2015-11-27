da98e066628898b26e500b5a278938597d716727
Jasper Haggenburg <jasperh@q42.nl>
2015-11-27 14:10:04 +0100

    animate stuff

--- chromeextension/src/content_scripts/visualize.js
+++ chromeextension/src/content_scripts/visualize.js
@@ -2,48 +2,72 @@ import _ from 'lodash';
 
 export default class Visualize {
   constructor() {
-    console.log('o')
     // Get results from API
-
-    const apiResults = [
-      {
-        url: 'https://medium.com/@lawham/phonetic-table-for-trolls-f03c05dc7f6a',
-        score: 1
-      }, {
-        url: 'https://medium.com/@jblake17/a-historic-day-for-the-world-of-competitive-rubik-s-cube-solving-33a92a5f3d1c',
-        score: 0
-      }, {
-        url: 'https://medium.com/@Asher_Wolf/here-s-to-not-hating-other-parents-b34ee8dd8c7d',
-        score: -1
-      },
-    ];
+    const apiResults = this.getApiResults();
 
     // Add classes to links
-
-    _.each(apiResults, apiResult => {
-      const domNodes = document.querySelectorAll(`[href^="${apiResult.url}"]`);
-
-      _.each(domNodes, domNode => {
-        domNode.classList.add('--bs--link');
-
-        switch (apiResult.score) {
-          case 0: domNode.classList.add('--bs--link-neutral'); break;
-          case 1: domNode.classList.add('--bs--link-positive'); break;
-          case -1: domNode.classList.add('--bs--link-negative'); break;
-        }
-      });
-    });
+    this.setupLinks(apiResults);
 
     // Toggle view
+    document.onkeydown = this.onKeyDown;
+    document.onkeyup = this.onKeyUp;
+  }
 
-    document.onkeydown = e => {
-      if(e.altKey) {
-        document.body.classList.add('--bs--enabled');
-      }
-    };
+  getApiResults() {
+    return [
+      // medium
+      {
+        url: 'https://medium.com/@lawham/phonetic-table-for-trolls-f03c05dc7f6a',
+        bullshit: 1
+      }, {
+        url: 'https://medium.com/@Asher_Wolf/here-s-to-not-hating-other-parents-b34ee8dd8c7d',
+        bullshit: 1
+      }, {
+        url: 'https://medium.com/@Asher_Wolf/here-s-to-not-hating-other-parents-b34ee8dd8c7d',
+        bullshit: 1
+      },
+      // nu.nl
+      {
+        url: '/binnenland/4173018/supporters-ajax-stappen-rechter-aanwezigheid-bij-fc-utrecht.html',
+        bullshit: 1
+      },
+      {
+        url: '/binnenland/4172847/kuipverbod-en-werkstraffen-geeist-feyenoordfans-in-rome-zaak.html',
+        bullshit: 1
+      },
+      {
+        url: '/politiek/4172867/akkoord-opvang-huisvesting-en-integratie-asielzoekers.html',
+        bullshit: 1
+      },
+    ];
+  }
 
-    document.onkeyup = e => {
-      document.body.classList.remove('--bs--enabled');
-    };
+  setupLinks(apiResults) {
+    _.each(apiResults, apiResult => {
+      const domNodes = document.querySelectorAll(`[href^="${apiResult.url}"]`);
+      _.each(domNodes, domNode => this.setupLink(apiResult, domNode));
+    });
+  }
+
+  setupLink(apiResult, domNode) {
+    domNode.classList.add('--bs--link');
+
+    if (apiResult.bullshit) {
+      domNode.classList.add('--bs--link-bullshit');
+    }
+
+    if (apiResult.interesting) {
+      domNode.classList.add('--bs--link-interesting');
+    }
+  }
+
+  onKeyDown(e) {
+    if(e.altKey) {
+      document.body.classList.add('--bs--enabled');
+    }
+  }
+
+  onKeyUp(e) {
+    document.body.classList.remove('--bs--enabled');
   }
 }
